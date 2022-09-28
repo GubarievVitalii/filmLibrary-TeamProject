03f9aad0f162e8a7c5e6740450d34d7a70e4ad4b
tarsweb <tarsweb@ukr.net>
2022-09-28 15:09:13 +0300

    Movies Api,  render trend

---
+++ src/js/MoviesApi.js
@@ -0,0 +1,86 @@
+import axios from "axios";
+const API_URL = 'https://api.themoviedb.org/3';
+const API_KEY = 'a620a6416fb18e40f7d335c64c3f9e0e';
+
+axios.defaults.baseURL = API_URL;
+
+function handlerGenres ({results, ...other}, genres) {
+  for (const object of results) {
+    object.genre_str = object.genre_ids.map(elem => genres.find(genre =>  genre.id === elem).name);
+  }
+  return ({...other, results})
+}
+
+class MoviesApi{
+
+  static genres = null;
+  static async fetchGendresMovie() {
+      const response = await axios.get("/genre/movie/list", {
+        params: {
+          api_key: API_KEY,
+        },
+      });
+      this.genres = response.data.genres;
+    }
+
+
+  #searchQuery;
+  #currentPage;
+
+  constructor () {
+      this.#searchQuery = null;
+      this.#currentPage = 1;
+
+      this.constructor.fetchGendresMovie();
+  }
+
+
+  async fetchTrendWeekMovies() {
+    const response = await axios.get(`/trending/movie/week`, {
+      params: {
+        api_key: API_KEY,
+        page: this.#currentPage,
+        language: "en",
+      },
+    });
+    return handlerGenres(response.data, MoviesApi.genres);
+  }
+
+  async fetchTrendDayMovies() {
+    const response = await axios.get(`/trending/movie/day`, {
+      params: {
+        api_key: API_KEY,
+        page: this.#currentPage,
+        language: "en",
+      },
+    });
+    return handlerGenres(response.data, MoviesApi.genres);
+  }
+
+  get query() {
+    return this.#searchQuery
+  }
+
+  set query(newSearhQuery) {
+    this.#searchQuery = newSearhQuery;
+    this.#currentPage = 1;
+  }
+
+  get currentpage() {
+    return this.#currentPage
+  }
+
+  set currentpage(newPage) {
+    this.#currentPage = newPage;
+  }
+
+  get genres() {
+    return MoviesApi.genres;
+  }
+
+  set genres (genre) {
+      return
+  }
+}
+
+export default MoviesApi;
---
+++ src/js/renderMarkup.js
@@ -0,0 +1,41 @@
+const pathImage = "https://image.tmdb.org/t/p/";
+
+export function createMarkupElement({id, title, poster_path, genre_str, release_date, vote_average}) {
+  return `<li class="galary-list__item">
+              <a data-modal-open href="" class="galary-list-link-wrapper" data-movieId = ${id}>
+                <div class="galary-list-item-wraper">
+                  <picture>
+                    <source
+                      srcset=${pathImage}w1280${poster_path}
+                      media="(min-width: 1280)"
+                      type="image"
+                    />
+                    <source
+                      srcset==${pathImage}w768${poster_path}
+                      media="(min-width: 768)"
+                      type="image"
+                    />
+                    <source
+                      srcset=${pathImage}w320${poster_path}
+                      media="(min-width: 320)"
+                      type="image"
+                    />
+                    <img
+                      src=${pathImage}original${poster_path}
+                      alt="galary"
+                      class="galary-list-item-img"
+                    />
+                  </picture>
+                  <div class="film-data first">
+                    <p class="films-name">${title}</p>
+                    <div class="film-data-wraper">
+                      <p class="films-genre">
+                        ${genre_str.join(", ")} &#124; ${release_date.slice(0,4)}
+                      </p>
+                      <p class="film-rating">${vote_average}</p>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </li>`
+}
