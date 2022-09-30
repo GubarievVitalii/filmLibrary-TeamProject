9cdf6737a455440318c5628c3fb908da5ce4c030
gubariev-vitalii <100084830+GubarievVitalii@users.noreply.github.com>
2022-09-30 18:19:57 +0300

    v3

--- src/js/get-refs.js
+++ src/js/get-refs.js
@@ -1,9 +1,10 @@
 export default function getRefs() {
-    return {
-        searchForm: document.querySelector('#search-form'),
-        searchInput: document.querySelector('.search-input'),
-        searchBtn: document.querySelector('.search-button'),
-        imagesContainer: document.querySelector('.galary-list'),
-    }
-  }
-
+  return {
+    searchForm: document.querySelector('#search-form'),
+    searchInput: document.querySelector('.search-input'),
+    searchBtn: document.querySelector('.search-button'),
+    imagesContainer: document.querySelector('.galary-list'),
+    checkBoxEl: document.querySelector('[name="ligthswitcher"]'),
+    bodyEl: document.querySelector('body'),
+  };
+}
